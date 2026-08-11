098263673ad0565d0a1f70c61cf1c916a0650434
fizza <your@email.com>
2026-08-11 22:02:19 +0500

    added user model

--- tutor/models.py
+++ tutor/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
+from django.db import models 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id} - {self.user.username}"
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20)
+    content = models.TextField
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.role} - {self.content[:30]}"
