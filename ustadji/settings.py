0738b1c3526a0dde393c92894435be5c15927c00
fizza <frizvi7212@gmail.com>
2026-09-04 16:44:13 +0500

    Add root redirect to /tutor/

--- ustadji/settings.py
+++ ustadji/settings.py
@@ -24,9 +24,12 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fizzafatima.pythonanywhere.com']
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Application definition
 
